6d7bf186885552d5170c72c1b2c908810a65d48a
Giordano Rodrigues <22085029+giordanorn@users.noreply.github.com>
2019-04-04 16:33:29 -0300

    Adicionado script de pulo do jogador

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
+        }
+    }
+}
