7c065c5482ac66cc193d7c6e3b9b7f70abfe0ca0
João Pedro Holanda <jpholanda.prf@gmail.com>
2019-05-09 17:22:11 -0300

    Adicionado plano de fundo e troca de tilesets do primeiro mapa
    Adicionado plano de fundo e tilesets retirados do opengameart (lembrar de colocar créditos). Iniciado design do primeiro mapa.

---
+++ Assets/Scripts/Parallax2DFixed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax2DFixed : MonoBehaviour
+{
+    private float deltaX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        deltaX = Camera.main.transform.position.x - transform.position.x; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (
+            Camera.main.transform.position.x - deltaX,
+            transform.position.y,
+            transform.position.z
+            );
+    }
+}
